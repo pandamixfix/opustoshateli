5549b8002b96885fd6afddb38eaa9e27b5b02497
Sergey <tsapenko02ss@gmail.com>
2026-05-02 21:40:53 +0300

    fix basedata2

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ДОБАВЛЯЕМ ВОТ ЭТОТ БЛОК:
+  images: {
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co', // Разрешаем прямые ссылки с Supabase
+      },
+      {
+        protocol: 'https',
+        hostname: 'opustoshateli.vercel.app', // Разрешаем твои абсолютные прокси-ссылки
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost', // На всякий случай для локальной разработки
+      }
+    ],
+  },
+  
+  // Твои редиректы оставляем как есть:
   async rewrites() {
     return[
       {
-        // Перехватываем все запросы, идущие на /supabase/...
         source: '/supabase/:path*',
-        // И незаметно перенаправляем их на реальный URL твоего проекта Supabase
         destination: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/:path*`,
       },
     ];
